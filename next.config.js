1a2161cc7bdab05be95faabe6a6ff548cdb47430
unizazang <zifnffk32123@gmail.com>
2025-03-08 17:42:38 +0900

    빌드 오류 해결

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const path = require("path"); // ✅ path 모듈 추가
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone", // ✅ 독립형 빌드로 설정
+
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, ".");
     config.externals = [...(config.externals || []), "canvas"];
@@ -8,8 +12,8 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     esmExternals: false,
-    serverActions: false,
-    appDir: true, // App Router 사용 시 반드시 true
+    // serverActions: false,
+    // appDir: true, // App Router 사용 시 반드시 true
     serverComponentsExternalPackages: ["canvas"],
   },
 };
