2d30e84a07fae8051e4788ddb2d304967ae09111
unizazang <zifnffk32123@gmail.com>
2025-03-08 16:38:32 +0900

    config 설정 바꿈 : esmExternals, config.resolve.alias 추가

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, ".");
     config.externals = [...(config.externals || []), "canvas"];
     return config;
   },
   reactStrictMode: true,
   experimental: {
+    esmExternals: false,
     serverActions: false,
     appDir: true, // App Router 사용 시 반드시 true
     serverComponentsExternalPackages: ["canvas"],
